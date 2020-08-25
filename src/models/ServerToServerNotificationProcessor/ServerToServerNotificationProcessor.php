ae7fa2a1c8397a384c4a78ecbd392ce22ff076e9
Dominik Skerhak <dominik.skerhak@gmail.com>
2020-08-25 12:02:21 +0200

    Refactor ServerToServerNotificationProcessor::getUser
    - Added search for user within `payment_meta`. This is always filled by
      `ServerToServerNotificationWebhookApiHandler::createPayment()`.
    - Moved search within `user_meta` on second place. This can be filled
      for imported users without payments or for testing purposes (sandbox
      payments).
    
    Preparation for changes needed by remp/crm#1235

--- src/models/ServerToServerNotificationProcessor/ServerToServerNotificationProcessor.php
+++ src/models/ServerToServerNotificationProcessor/ServerToServerNotificationProcessor.php
@@ -4,6 +4,7 @@ namespace Crm\AppleAppstoreModule\Model;
 
 use Crm\AppleAppstoreModule\AppleAppstoreModule;
 use Crm\AppleAppstoreModule\Repository\AppleAppstoreSubscriptionTypesRepository;
+use Crm\PaymentsModule\Repository\PaymentMetaRepository;
 use Crm\UsersModule\Repository\UserMetaRepository;
 use Nette\Database\Table\ActiveRow;
 
@@ -13,13 +14,17 @@ class ServerToServerNotificationProcessor implements ServerToServerNotificationP
 
     private $appleAppstoreSubscriptionTypesRepository;
 
+    private $paymentMetaRepository;
+
     private $userMetaRepository;
 
     public function __construct(
         AppleAppstoreSubscriptionTypesRepository $appleAppstoreSubscriptionTypesRepository,
+        PaymentMetaRepository $paymentMetaRepository,
         UserMetaRepository $userMetaRepository
     ) {
         $this->appleAppstoreSubscriptionTypesRepository = $appleAppstoreSubscriptionTypesRepository;
+        $this->paymentMetaRepository = $paymentMetaRepository;
         $this->userMetaRepository = $userMetaRepository;
     }
 
@@ -37,23 +42,38 @@ class ServerToServerNotificationProcessor implements ServerToServerNotificationP
     }
 
     /**
-     * @inheritDoc
+     * getUser returns User from Apple's ServerToServerNotification.
+     *
+     * - User is searched by original_transaction_id linked to previous payments (payment_meta).
+     * - User is searched by original_transaction_id linked to user itself (user_meta).
+     *
+     * @return ActiveRow $user
      */
     public function getUser(ServerToServerNotification $serverToServerNotification): ActiveRow
     {
         $originalTransactionId = $serverToServerNotification->getUnifiedReceipt()->getLatestReceiptInfo()->getOriginalTransactionId();
+
+        // search user by `original_transaction_id` linked to payment
+        $paymentsWithMeta = $this->paymentMetaRepository->findAllByMeta(
+            AppleAppstoreModule::META_KEY_ORIGINAL_TRANSACTION_ID,
+            $originalTransactionId
+        );
+        if (!empty($paymentsWithMeta)) {
+            return reset($paymentsWithMeta)->payment->user;
+        }
+
+        // search user by `original_transaction_id` linked to user itself (eg. imported iOS users without payments in CRM)
         $usersMetas = $this->userMetaRepository->usersWithKey(
             AppleAppstoreModule::META_KEY_ORIGINAL_TRANSACTION_ID,
             $originalTransactionId
         )->fetchAll();
-        if (empty($usersMetas)) {
-            throw new \Exception("No user found with provided original transaction ID [{$originalTransactionId}].");
-        }
         if (count($usersMetas) > 1) {
             throw new \Exception("Multiple users with same original transaction ID [{$originalTransactionId}].");
         }
+        if (!empty($usersMetas)) {
+            return reset($usersMetas)->user;
+        }
 
-        $userMeta = reset($usersMetas);
-        return $userMeta->user;
+        throw new \Exception("No user found with provided original transaction ID [{$originalTransactionId}].");
     }
 }
