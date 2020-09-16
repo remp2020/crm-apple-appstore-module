c3b2c9190031a6765850468e561b5b050fe2fb13
Peter Dulacka <dulacka@gmail.com>
2020-09-16 14:46:50 +0200

    Add migration for apple appstore receipts storage

---
+++ src/migrations/20200916120018_apple_appstore_receipts.php
@@ -0,0 +1,17 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AppleAppstoreReceipts extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table('apple_appstore_receipts')
+            ->addColumn('original_transaction_id', 'string', ['null' => 'false'])
+            ->addColumn('receipt', 'text', ['null' => false])
+            ->addColumn('created_at', 'datetime', ['null' => false])
+            ->addColumn('updated_at', 'datetime', ['null' => false])
+            ->addIndex('original_transaction_id', ['unique' => true])
+            ->create();
+    }
+}
