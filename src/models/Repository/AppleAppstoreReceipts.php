3f423b99b97d53600d7848dba056a401e57d4428
Peter Dulacka <dulacka@gmail.com>
2020-09-16 14:38:50 +0200

    Added storage to store receipts for each original transaction ID
    These receipts are required for checking subscription status
    and renewals during recurring payment process.

---
+++ src/models/Repository/AppleAppstoreReceipts.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Crm\AppleAppstoreModule\Repository;
+
+use Crm\ApplicationModule\Repository;
+use Nette\Database\Table\IRow;
+use Nette\Utils\DateTime;
+
+class AppleAppstoreReceipts extends Repository
+{
+    protected $tableName = 'apple_appstore_receipts';
+
+    final public function add(string $originalTransactionId, string $receipt)
+    {
+        $row = $this->findByOriginalTransactionId($originalTransactionId);
+        if ($row) {
+            $this->update($row, [
+                'receipt' => $receipt,
+            ]);
+            return $row;
+        }
+
+        $now = new DateTime();
+        return $this->getTable()->insert([
+            'original_transaction_id' => $originalTransactionId,
+            'receipt' => $receipt,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+    }
+
+    final public function findByOriginalTransactionId(string $originalTransactionId)
+    {
+        return $this->getTable()->where(['original_transaction_id' => $originalTransactionId])->fetch();
+    }
+
+    final public function update(IRow &$row, $data)
+    {
+        $data['updated_at'] = new DateTime();
+        return parent::update($row, $data);
+    }
+}
